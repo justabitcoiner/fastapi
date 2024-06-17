b1ade55d86cd53626ca6424ed53b927c4ff04f82
justabitcoiner <justabitcoiner@gmail.com>
2024-06-18 01:58:46 +0700

    improve UX in json response

--- src/schemas/response.py
+++ src/schemas/response.py
@@ -2,6 +2,6 @@ from sqlmodel import SQLModel, Field
 
 
 class JsonResponse(SQLModel):
-    data: dict = Field(default={})
-    message: str = Field(default="")
     status: int = Field(default=200)
+    message: str = Field(default="")
+    data: dict = Field(default={})
