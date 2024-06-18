793a0ddbcabf0de398b98e601987423948eefb51
justabitcoiner <justabitcoiner@gmail.com>
2024-06-18 22:27:40 +0700

    add exception handler for Exception class

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 from src.config import Configuration
 from src.jwt import Jwt
 from src import models
 from src.db import Engine
 from src.routers import user, token
+from src.schemas.response import JSONResponseContent
 
 config = Configuration.get_config()
 Jwt.gen_secret_key()
@@ -21,3 +23,8 @@ async def use_session(req: Request, call_next):
     with Engine.get_session() as session:
         req.state.session = session
         return await call_next(req)
+
+
+@app.exception_handler(Exception)
+def handle_exception(request: Request, exc: Exception):
+    return JSONResponse(JSONResponseContent(message=str(exc)).model_dump())
